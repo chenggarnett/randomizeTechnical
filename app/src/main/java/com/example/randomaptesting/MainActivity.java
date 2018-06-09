4af77b76819ca75c5bd045009b516e0131a103ab
chenggarnett <chengchin5227@gmail.com>
2018-06-09 11:22:34 -0700

    became dynamic

--- app/src/main/java/com/example/randomaptesting/MainActivity.java
+++ app/src/main/java/com/example/randomaptesting/MainActivity.java
@@ -4,13 +4,13 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,8 +22,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -32,15 +30,9 @@ import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -49,22 +41,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
  * Created by chengchinlim on 5/29/18.
  */
 
-public class MainActivity extends FragmentActivity implements OnConnectionFailedListener {
+public class MainActivity extends FragmentActivity {
 
-    protected GeoDataClient mGeoDataClient;
-    protected PlaceDetectionClient mPlaceDetectionClient;
-    protected GoogleApiClient mGoogleApiClient;
-    protected FusedLocationProviderClient mFusedLocationProviderClient;
-    int PLACE_PICKER_REQUEST = 1;
-    private GoogleMap mMap;
-    public static final String TAG = "CurrentLocNearByPlaces";
+    private FusedLocationProviderClient mFusedLocationClient;
+    protected Location mLastLocation;
     private static final int LOC_REQ_CODE = 1;
+    protected ArrayList<String> addressList;
+    protected ArrayList<String>  nameList;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,136 +62,110 @@ public class MainActivity extends FragmentActivity implements OnConnectionFailed
 
         final TextView mTextView = findViewById(R.id.text);
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.334954,-121.880571&radius=3000&type=restaurant&keyword=thailand&key=AIzaSyDpKpQ2S8lvUK7xfHGgSoJXy0HG9tFU-7s";
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+    }
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-            new Response.Listener<String>() {
+    @SuppressWarnings("MissingPermission")
+    private void getLastLocation() {
+        mFusedLocationClient.getLastLocation()
+            .addOnCompleteListener(this, new OnCompleteListener<Location>() {
                 @Override
-                public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-                    //mTextView.setText("Response is: "+ response.substring(0,5000));
-                    //System.out.println(response.toString());
+                public void onComplete(@NonNull Task<Location> task) {
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        mLastLocation = task.getResult();
+                        double latitude = mLastLocation.getLatitude();
+                        double longitude = mLastLocation.getLongitude();
+                        String result = "Last known Location Latitude is " +
+                                mLastLocation.getLatitude() + "\n" +
+                                "Last known longitude Longitude is " + mLastLocation.getLongitude();
+                        System.out.println(result);
+                        String completeUrl = constructUrl(latitude, longitude);
+                        System.out.println(completeUrl);
+                        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                        StringRequest stringRequest = new StringRequest(Request.Method.GET, completeUrl,
+                                new Response.Listener<String>() {
+                                    @Override
+                                    public void onResponse(String response) {
+                                        try {
+                                            JSONObject obj = new JSONObject(response);
+                                            JSONArray results = obj.getJSONArray("results");
 
-                    try {
-                        JSONObject obj = new JSONObject(response);
-                        JSONArray results = obj.getJSONArray("results");
-
-                        ArrayList<String> listdata = new ArrayList<String>();
-                        JSONArray jArray = (JSONArray)results;
-                        if (jArray != null) {
-                            for (int i=0;i<jArray.length();i++){
-                                listdata.add(jArray.getString(i));
+                                            ArrayList<String> listData = new ArrayList<String>();
+                                            JSONArray jArray = results;
+                                            if (jArray != null) {
+                                                for (int i=0;i<jArray.length();i++){
+                                                    listData.add(jArray.getString(i));
+                                                }
+                                            }
+                                            nameList =  new ArrayList<>(getNameList(listData));
+                                            addressList = new ArrayList<> (getAddressList(listData));
+                                            for (String s : nameList) {
+                                                System.out.println("Name: " + s);
+                                            }
+                                            for (String s: addressList) {
+                                                System.out.println("Address: " + s);
+                                            }
+                                            Uri gmmIntentUri = Uri.parse("google.navigation:q=" + addressList.get(0) + ",San+Jose,United+States");
+                                            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                            mapIntent.setPackage("com.google.android.apps.maps");
+                                            startActivity(mapIntent);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println("Volley Error");
                             }
                         }
+                        );
 
-                        for (String s: listdata) {
-                            //System.out.println(s.toString());
-                            int a = s.indexOf("vicinity");
-                            String s1 = s.substring(a + 11);
-                            s1 = s1.substring(0, s1.length() - 2);
-                            s1 = s1.replace(' ', '+');
-                            System.out.println(s1);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                        queue.add(stringRequest);
+                    } else {
+                        System.out.println("No Last known location found. Try current location..!");
                     }
-
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    mTextView.setText("String request 1 didn't work!");
-                }
-            }
-        );
-
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=San+Jose+Airport,San+Jose,United+States");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_maps);
-////        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-////                .findFragmentById(R.id.map);
-////        mapFragment.getMapAsync(this);
-//
-//        // Construct a GeoDataClient.
-//        mGeoDataClient = Places.getGeoDataClient(this, null);
-//
-//        // Construct a PlaceDetectionClient.
-//        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
-//
-////      // TODO: Start using the Places API.
-//        // Construct a FusedLocationProviderClient.
-//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-//
-//        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-//
-//        if (isLocationAccessPermitted()) {
-//            getCurrentPlaceData();
-//        } else {
-//            requestLocationAccessPermission();
-//        }
-//
-//        try {
-//            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
-//        } catch (GooglePlayServicesRepairableException e) {
-//            e.printStackTrace();
-//        } catch (GooglePlayServicesNotAvailableException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(this, "onConnectionFailed!!!", Toast.LENGTH_LONG).show();
+            });
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PLACE_PICKER_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Result code is not ok!!!", Toast.LENGTH_LONG).show();
-            }
-        } else {
-            Toast.makeText(this, "Wrong request code!!!", Toast.LENGTH_LONG).show();
+    private String constructUrl(double latitude, double longitude) {
+        String basicUrl ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+        String latAndLong = "location=" + Double.toString(latitude) + "," + Double.toString(longitude);
+        String radiusFrCurrentLocation = "&radius=3000";
+        String placeType = "&type=restaurant";
+        String keyword = "&keyword=food&opennow=1";
+        String apiKey = "&key=AIzaSyDpKpQ2S8lvUK7xfHGgSoJXy0HG9tFU-7s";
+        String completeUrl = basicUrl + latAndLong + radiusFrCurrentLocation
+                + placeType + keyword + apiKey;
+        return completeUrl;
+    }
+
+    private HashSet<String> getAddressList(ArrayList<String> listData) {
+        HashSet<String> addressList = new HashSet<>();
+        for (String s: listData) {
+            System.out.println(s.toString());
+            int a = s.indexOf("vicinity");
+            int b = s.length();
+            String s1 = s.substring(a + 11, b - 2);
+            s1 = s1.replace(' ', '+');
+            addressList.add(s1);
         }
+        return addressList;
     }
 
-    private void getCurrentPlaceData() {
-        @SuppressLint("MissingPermission")
-        Task<PlaceLikelihoodBufferResponse> placeResult = mPlaceDetectionClient
-                .getCurrentPlace(null);
-
-        placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
-            @Override
-            public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
-                Log.d(TAG, "current location places info");
-                List<Place> placesList = new ArrayList<>();
-                PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
-                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
-                    placesList.add(placeLikelihood.getPlace().freeze());
-                }
-                for (Place place: placesList) {
-                    System.out.println("Place Name: " + place.getName());
-                    System.out.println("Place Type: " + place.getPlaceTypes());
-                }
-                likelyPlaces.release();
-
-            }
-        });
+    private HashSet<String> getNameList(ArrayList<String> listData) {
+        HashSet<String> nameList = new HashSet<>();
+        for (String s: listData) {
+            //System.out.println(s.toString());
+            int a = s.indexOf("name");
+            int b = s.indexOf("opening");
+            String s2 = s.substring(a + 7, b - 3);
+            s2 = s2.replace(' ', '+');
+            nameList.add(s2);
+        }
+        return nameList;
     }
 
     private boolean isLocationAccessPermitted() {
